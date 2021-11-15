b0f69c8129f57b6bce40d6afa280f49b9f28f0f4
sebastian rowland <sebastianrowland@sebastians-mbp.myfiosgateway.com>
2021-11-15 07:25:19 -0500

    Added toy_Analyst outputs

--- scripts/g_15_numbers_for_Results.R
+++ scripts/g_15_numbers_for_Results.R
@@ -200,19 +200,97 @@ c.4.EEper05toMeanInd14iceQ234 <- est.table.cumul[14, 2]
 #c.3.EEper05toMeanInd14iceQ1 <- est.table$estCumul_16.4_iceQ1[14]
 #c.4.EEper05toMeanInd14iceQ234 <- est.table$estCumul_16.4_iceQ234[14]
 
-# 3i Save numeric results 
+
+####*************************************
+#### 4: Numeric Results for Abstract ####
+####*************************************
+
+# 4a Setup table of models to tabulate
+sensitivityList <- rep("main", 8)
+subSetVarList <- rep("season", 8)
+subSetList <- c(rep("fal", 2), rep("win", 2), rep("spr", 2), rep("sum", 2))
+ERConstraintList <- rep("selectedMain", 8)
+LRConstraintList <- rep("selectedMain", 8)
+indCumulList <- rep(c("estInd", "estCumul"), 4)
+
+# 4b Readin estimates
+est.table <- purrr::pmap(
+  list(
+    sensitivityList, subSetVarList, subSetList,
+    ERConstraintList, LRConstraintList, indCumulList
+  ),
+  readEstimates
+) %>% 
+  bind_rows()
+
+# 4c Keep only the exposure contrasts of interest
+est.table <- est.table %>%
+  filter(refT == contrastB[1][[1]] & label == contrastB[2][[1]])
+
+# 4d Round counterfactual_temp so that it is presentable
+est.table <- est.table %>%
+  mutate(counterfactual_temp = round(counterfactual_temp, 2))
+
+# 4e As usual, wrangle the estimates
+est.table <- est.table %>%
+  dplyr::select(-sensitivity, -subSetVar, -label, -refT) %>%
+  gather("lag_name", "estimate", -counterfactual_temp, -subSet, -indCumul) %>%
+  mutate(var_name = str_sub(lag_name, 1, 3), lag_day = as.numeric(str_sub(lag_name, 11))) %>%
+  dplyr::select(-lag_name) %>%
+  filter(!str_detect(lag_day, "\\.")) %>%
+  spread(var_name, estimate) %>%
+  mutate(lag_day = as.character(str_pad(lag_day, 2, "left", "0"))) %>%
+  mutate(
+    fit_pc = convertToPercent(fit),
+    lci_pc = convertToPercent(lci),
+    uci_pc = convertToPercent(uci)
+  ) %>%
+  mutate(se_pc = (uci_pc - lci_pc) / 1.96)
+
+# 4f Put table in a tidy format, with 3 columns
+est.table <- est.table %>%
+  mutate(EE = paste0(
+    format(round(fit_pc, 1), nsmall = 1), " (",
+    format(round(lci_pc, 1), nsmall = 1), ", ",
+    format(round(uci_pc, 1), nsmall = 1), ")"
+  ))
+
+# 4g Arrange the effect estimates so we have one exposure contrast per column
+est.table <- est.table %>%
+  mutate(EXPName = paste0(indCumul, "_", counterfactual_temp, "_", subSet)) %>%
+  dplyr::select(EXPName, lag_day, EE) %>%
+  spread(EXPName, EE) %>%
+  dplyr::select(lag_day, contains("Ind"), contains("Cumul"))
+
+# 4h Extract effect estimates of interest 
+eeFall <- est.table %>% 
+  dplyr::select(contains('Ind')) %>% 
+  dplyr::select(contains('fal'))
+d.a.eeFall <- eeFall[1,1] 
+eeSpring <- est.table %>% 
+  dplyr::select(contains('Ind')) %>% 
+  dplyr::select(contains('spr'))
+d.b.eeSpring <- eeSpring[1,1] 
+
+####*************
+#### 5: Save ####
+####*************
+
+# 5 Save numeric results 
 data.frame(quantity = c(
   'a.1.percentfemale', 'a.2.winningQAIC',
   'b.1.EEper05toMeanInd0', 'b.2.EEmeanto95Ind0', 'b.3.EEper05toMeanCumul14',
   'b.4.EEmeanto95Cumul14', 'b.5.EEfig2caption1', 'b.6.EEfig2caption2', 
   'c.1.EEper05toMeanInd14kpsc', 'c.2.EEper05toMeanInd14Sutter', 
-  'c.3.EEper05toMeanInd14iceQ1', 'c.4.EEper05toMeanInd14iceQ234'), 
+  'c.3.EEper05toMeanInd14iceQ1', 'c.4.EEper05toMeanInd14iceQ234', 
+  'd.a.eeFall', 'd.b.eeSpring'), 
            value = c(
              a.1.percentfemale, a.2.winningQAIC,
              b.1.EEper05toMeanInd0, b.2.EEmeanto95Ind0, b.3.EEper05toMeanCumul14, 
              b.4.EEmeanto95Cumul14, b.5.EEfig2caption1, b.6.EEfig2caption2, 
              c.1.EEper05toMeanInd14kpsc, c.2.EEper05toMeanInd14Sutter, 
-             c.3.EEper05toMeanInd14iceQ1, c.4.EEper05toMeanInd14iceQ234)) %>% 
+             c.3.EEper05toMeanInd14iceQ1, c.4.EEper05toMeanInd14iceQ234, 
+             d.a.eeFall, d.b.eeSpring)) %>% 
   write_csv(here::here(outPath, "manuscript", 'numeric_results.csv'))
   
 
